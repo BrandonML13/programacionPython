8313489dcc427ac5fa7eef77d7ab3bf20c1ecdea
BrandonML13 <brandonenp2@gmail.com>
2025-03-05 15:49:07 -0600

    MenuBanco.py

--- teoria/relacionesDeClases/MenuBanco.py
+++ teoria/relacionesDeClases/MenuBanco.py
@@ -1,7 +1,19 @@
 class MenuBanco:
-    def __init__(self,banco):
+    def __init__(self,banco,opcion=None):
         self.bienvenida = banco
-    
+        self.opcion = opcion
     def darBienvenida(self):
-        print("Bienvenid@ a su banco de confianza,"+ banco1.bienvenida)
+        print("Bienvenid@ a su banco de confianza, "+ banco1.bienvenida)
         print()
+    def desplegarOpciones(self):
+        print("¿Que le gustaria hacer?")
+        print("1.- depositar")
+        print("2.- retirar")
+        self.opcion = input("ingrese el numero de la acción que desea realizar:")
+    def realizaAccion(self):
+        if self.opcion == "1":
+          cuenta1.depositar(float(input()))
+        if self.opcion == "2":
+           cuenta1.retirar(float(input()))
+        else:
+          print("opcion no valida por favor vuelva a ingresar un numero valido")
