be477cd05a44bda903df94c7999f7f1e1f48e39b
BrandonML13 <brandonenp2@gmail.com>
2025-03-12 17:37:22 -0600

    Update MenuBanco.py

--- teoria/relacionesDeClases/MenuBanco.py
+++ teoria/relacionesDeClases/MenuBanco.py
@@ -1,3 +1,6 @@
+#Las ultimas modificacones del codigo las realice el miercoles confio que me esta quedando bastante bien, lo ultimo que me falta es meter las conexiones logicas
+#Para los metodos de depositar y retirar
+#Autor: Brandon Martinez Lopez 
 class MenuBanco:
     def __init__(self,banco,opcion=None):
         self.bienvenida = banco
@@ -8,19 +11,29 @@ class MenuBanco:
     def desplegarOpciones(self):
         MenuBanco.estadoCuenta(self)
         print("¿Que le gustaria hacer?")
-        print("1.- depositar")
-        print("2.- retirar")
-        print("3.- salir")
+        print("1.- Ver detalles de mi cuenta")
+        print("2.- depositar")
+        print("3.- retirar")
+        print("4.- salir")
         self.opcion = input("ingrese el numero de la acción que desea realizar:")
+
     def realizarAccion(self):
         while True:
             self.desplegarOpciones()
           
             if self.opcion == "1":
-                MenuBanco.depositar(self)
+                print()
+                MenuBanco.imprimirDetalles(self)
+
             elif self.opcion == "2":
-                MenuBanco.retirar(self)
+                print()
+                MenuBanco.depositar(self)
+
             elif self.opcion == "3":
+                print()
+                MenuBanco.retirar(self)
+          
+            elif self.opcion == "4":
                 print()
                 print("Gracias por su confianza en Banciencias, vuelva pronto")
                 break 
@@ -30,9 +43,16 @@ class MenuBanco:
         
     def depositar(self):
           cuenta1.depositar(float(input("Introduzca la cantidad que desea depositar")))
+          print("Su deposito se ha realizado con exito")
+          print()
 
     def retirar(self):
           cuenta1.retirar(float(input("Introduzca la cantidad que desea retirar")))
+          print("Su retiro se ha realizado con exito")
+          print()
+
+    def imprimirDetalles(self):
+        cliente1.imprimirDetalles()
           
     def estadoCuenta(self):
         print("su saldo actual es de =",cuenta1.saldo)
