de39006cb139a0869112bd4304cd55619e028186
BrandonML13 <brandonenp2@gmail.com>
2025-03-12 17:34:52 -0600

    Update Cliente.py

--- teoria/relacionesDeClases/Cliente.py
+++ teoria/relacionesDeClases/Cliente.py
@@ -1,12 +1,14 @@
 class Cliente:
-    def __init__(self,nombre,direccion,edad):
+    def __init__(self,nombre,direccion,edad,c):
         self.nombrePropietario = nombre
         self.direccionPropietario = direccion
         self.edadPropietario = edad
+        self.cuenta = c
     def imprimirDetalles(self):
         print("La informacion del cliente es:")
         print("nombre:", self.nombrePropietario)
         print("edad:", self.edadPropietario)
         print("direccion:", self.direccionPropietario)
+        print(self.cuenta.imprimirDetalles())
         print()
-
+     
