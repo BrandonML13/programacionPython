28ad19c0daaa8e858610ec5efb957cbec727303f
BrandonML13 <brandonenp2@gmail.com>
2025-03-26 11:14:25 -0600

    Update Cliente.py

--- teoria/relacionesDeClases/Cliente.py
+++ teoria/relacionesDeClases/Cliente.py
@@ -1,14 +1,16 @@
 class Cliente:
     def __init__(self,nombre,direccion,edad,c):
-        self.nombrePropietario = nombre
-        self.direccionPropietario = direccion
-        self.edadPropietario = edad
-        self.cuenta = c
+        self.__nombre = nombre
+        self.__direccion = direccion
+        self.__edad = edad
+        self.cuenta = c #dejo este atributo publico porque se ingresa directamente en el main
+
     def imprimirDetalles(self):
         print("La informacion del cliente es:")
-        print("nombre:", self.nombrePropietario)
-        print("edad:", self.edadPropietario,"años")
-        print("direccion:", self.direccionPropietario)
+        print("nombre:", self.__nombre)
+        print("edad:", self.__edad,"años")
+        print("direccion:", self.__direccion)
         print(self.cuenta.imprimirDetalles())
         print()
-     
+
+cliente1 = Cliente("Brandon Marlo","Roma,La petit cita #59,C.P 08343",35,cuenta1)
