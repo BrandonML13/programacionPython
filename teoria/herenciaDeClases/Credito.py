46ac69e791cf2266fad12ab8f68a7ff2635d9838
BrandonML13 <brandonenp2@gmail.com>
2025-05-08 00:24:23 -0600

    Create Credito.py

---
+++ teoria/herenciaDeClases/Credito.py
@@ -0,0 +1,11 @@
+class CuentaCredito(Madre):
+    def __init__(self, valor, limite_credito):
+        super().__init__(valor, "Crédito")
+        self.__limite_credito = limite_credito
+    
+    def __str__(self):
+        disponible = self.__limite_credito + self._Madre__valor  # Saldo + límite
+        return (f" Cuenta de Crédito | "
+                f"Saldo: ${self._Madre__valor:.2f} | "
+                f"Límite: ${self.__limite_credito:.2f} | "
+                f"Disponible: ${disponible:.2f}")
