280a140f9415e4b81f1afd99efaeacfc3495b84d
BrandonML13 <brandonenp2@gmail.com>
2025-05-08 00:28:43 -0600

    Update Credito.py

--- teoria/herenciaDeClases/Credito.py
+++ teoria/herenciaDeClases/Credito.py
@@ -5,7 +5,7 @@ class CuentaCredito(Madre):
         super().__init__(valor, "Crédito")
         self.__limite_credito = limite_credito
     
-    def __str__(self):
+    def __str__(self): #reescribi el codigo str de esta manera porque lo vi correcto ya que las tarjetas de credito te deben enseñar mas datos ademas de solo tu saldo
         disponible = self.__limite_credito + self._Madre__valor  # Saldo + límite
         return (f" Cuenta de Crédito | "
                 f"Saldo: ${self._Madre__valor:.2f} | "
