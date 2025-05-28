b900ae5db33f3553abe124308bf3464de1ea610d
BrandonML13 <brandonenp2@gmail.com>
2025-05-28 17:10:38 -0600

    Update Madre.py

--- teoria/herenciaDeClases/Madre.py
+++ teoria/herenciaDeClases/Madre.py
@@ -1,22 +1,34 @@
-class Madre:
-    def __init__(self,valor,tipo):
-        self.__valor = valor
-        self.__tipo = tipo
-
-    def retirar(self, cantidad):
-        if cantidad <= 0:
-            print("Error: La cantidad a retirar debe ser positiva.")  #Aun falta ajustar el codigo para que funcione bien en retirar 
-        if self.__valor - cantidad < 0:
-            print("Error: Saldo insuficiente.")
-        self.__valor -= cantidad
-        print(" Retiro exitoso. Saldo actual: $",self.__valor)
-  
+# Clase madre para todas las cuentas bancarias
+class Cuenta:
+    def __init__(self, titular, saldo=0):
+        self.__titular = titular 
+        self.__saldo = saldo
+    
+    # Decorador property para tener un codigo màs limpio
+    @property
+    def titular(self):
+        return self.__titular  
+    
+    
+    @property
+    def saldo(self):
+        return self.__saldo   
     def depositar(self, cantidad):
-        if cantidad <= 0:
-            print("La cantidad debe ser positiva.")
-        self.__valor += cantidad
-        
-    def __str__(self):
-        return f"Cuenta tipo: {self.__tipo}, Saldo disponible: ${self.__valor:.2f}"
-
+        if cantidad > 0:  # Validar que la cantidad sea positiva
+            self.__saldo += cantidad  # Sumar al saldo actual
+            return True  # Indicar éxito en la operación
+        return False  # Indicar fallo en la operación
+    
+    
+    def retirar(self, cantidad):
+        # Valido que la cantidad sea positiva y que haya saldo suficiente
+        if cantidad > 0 and cantidad <= self.__saldo:
+            self.__saldo -= cantidad  # Restar del saldo actual
+            return True 
+        return False  
+    
+    
+    def imprimir_detalles(self):
+        print(f"Titular: {self.__titular}") 
+        print(f"Saldo: ${self.__saldo:.2f}") 
 
